77bf6d6219262092e866e35eea5412bd2d4f784a
L-BLACK <rgb000000black@gmail.com>
2019-04-21 23:36:47 +0800

    PE改为自动化测试，目前500次测试没有问题

--- src/test/scala/pe/PETest.scala
+++ src/test/scala/pe/PETest.scala
@@ -123,85 +123,111 @@ class PETopTester(c: PETesterTop) extends PeekPokeTester(c) {
   }
 }
 
-class testModen(c: PETesterTop, filter: List[Int], filterNum: Int, img: List[Int], imgNum: Int, nchannel: Int)
+class testModen(c: PETesterTop, var loop: Int)
   extends PeekPokeTester(c) {
-  //  val pe = new simulator.PE()
-  //  pe.set(
-  //    DenseVector(filter.toArray), filterNum,
-  //    DenseVector(img.toArray), imgNum,
-  //    nchannel
-  //  )
-  //  val sw = pe.cal
-  //  println(pe.cal)
-  // first let PE in idle state
-  val swDM = SW.convGeneral(DenseVector(filter: _*), filterNum, DenseVector(img: _*), imgNum, nchannel)
-  println("SW: \n" + swDM.toString())
-  val sw = List[Int]().toBuffer
-  for (i <- Range(0, imgNum)) {
-    swDM(i * filterNum to (i + 1) * filterNum - 1, ::).flatten().toArray.toList.foreach(sw.append(_))
-    //    println(swDM(i * filterNum to (i + 1) * filterNum - 1, ::).flatten().toArray.toList.toString())
-  }
-  poke(c.io.stateSW, 0)
-  step(1) // because PE buf state, so need 1 clock
+  do {
+    var filter = List(1, 2, 3)
+    var img = List(1, 2, 3, 4, 5)
+    var filterNum = 1
+    var imgNum = 1
+    var nchannel = 1
+    val random = scala.util.Random
+    var mode = 1
+    do {
+      nchannel = random.nextInt(32) + 1
+      var fNum = random.nextInt(32) + 1
+      var iNum = random.nextInt(32) + 1
+      var fLen = random.nextInt(32) + 1
+      var iLen = random.nextInt(32) + fLen
+      var filter2d = DenseMatrix.fill[Int](fNum * nchannel, fLen)(random.nextInt(10) - 5)
+      var img2d = DenseMatrix.fill[Int](iNum * nchannel, iLen)(random.nextInt(10) - 5)
+      filter = filter2d.toArray.toList
+      img = img2d.toArray.toList
+      filterNum = fNum
+      imgNum = iNum
+    } while (filter.length > 255 | img.length > 255)
 
-  // second send basic infotmation to PE, include filterNum, singleFilterLen, imgNum, singleImgLen, nchannel
-  poke(c.io.peconfig.filterNum, filterNum)
-  poke(c.io.peconfig.singleFilterLen, filter.length / (filterNum * nchannel))
-  poke(c.io.peconfig.imgNum, imgNum)
-  poke(c.io.peconfig.singleImgLen, img.length / (imgNum * nchannel))
-  poke(c.io.peconfig.nchannel, nchannel)
-  step(1) // PE buf basic infotmation after 1 clock
+    //  val pe = new simulator.PE()
+    //  pe.set(
+    //    DenseVector(filter.toArray), filterNum,
+    //    DenseVector(img.toArray), imgNum,
+    //    nchannel
+    //  )
+    //  val sw = pe.cal
+    //  println(pe.cal)
+    // first let PE in idle state
+    val swDM = SW.convGeneral(DenseVector(filter: _*), filterNum, DenseVector(img: _*), imgNum, nchannel)
+    println("SW: \n" + swDM.toString())
+    val sw = List[Int]().toBuffer
+    for (i <- Range(0, imgNum)) {
+      swDM(i * filterNum to (i + 1) * filterNum - 1, ::).flatten().toArray.toList.foreach(sw.append(_))
+      //    println(swDM(i * filterNum to (i + 1) * filterNum - 1, ::).flatten().toArray.toList.toString())
+    }
+    poke(c.io.stateSW, 0)
+    step(1) // because PE buf state, so need 1 clock
 
-  // third put data in
-  poke(c.io.pSumIn.valid, 0)
-  poke(c.io.pSumIn.bits, 0)
-  poke(c.io.filter.valid, 0)
-  poke(c.io.img.valid, 0)
-  filter.foreach((num) => {
-    poke(c.io.filter.valid, 1)
-    poke(c.io.filter.bits, num)
+    // second send basic infotmation to PE, include filterNum, singleFilterLen, imgNum, singleImgLen, nchannel
+    poke(c.io.peconfig.filterNum, filterNum)
+    poke(c.io.peconfig.singleFilterLen, filter.length / (filterNum * nchannel))
+    poke(c.io.peconfig.imgNum, imgNum)
+    poke(c.io.peconfig.singleImgLen, img.length / (imgNum * nchannel))
+    poke(c.io.peconfig.nchannel, nchannel)
+    step(1) // PE buf basic infotmation after 1 clock
+
+    // third put data in
+    poke(c.io.pSumIn.valid, 0)
+    poke(c.io.pSumIn.bits, 0)
+    poke(c.io.filter.valid, 0)
+    poke(c.io.img.valid, 0)
+    filter.foreach((num) => {
+      poke(c.io.filter.valid, 1)
+      poke(c.io.filter.bits, num)
+      step(1)
+    })
+    poke(c.io.filter.valid, 0)
+    img.foreach((num) => {
+      poke(c.io.img.valid, 1)
+      poke(c.io.img.bits, num)
+      step(1)
+    })
+    poke(c.io.img.valid, 0)
     step(1)
-  })
-  poke(c.io.filter.valid, 0)
-  img.foreach((num) => {
-    poke(c.io.img.valid, 1)
-    poke(c.io.img.bits, num)
+
+    // fourth let PE in getdata state
+    poke(c.io.stateSW, 1)
+    step(filter.length + 1)
+
+    // finial let PE in cal state
+    poke(c.io.stateSW, 2)
     step(1)
-  })
-  poke(c.io.img.valid, 0)
-  step(1)
+    var j = 0
+    poke(c.io.oSum.ready, 1)
+    for (i <- Range(0, 40000)) {
+      //    println("oSum.bits: " + peek(c.io.oSum.valid).toString())
+      //    println("sw.cols: " + sw.cols.toString())
+      //    println("sw.rows: " + sw.rows.toString)
+      if (peek(c.io.oSum.valid) == 1) {
+        //      println(peek(c.io.oSum.bits).toString())
+        expect(c.io.oSum.bits, sw(j))
+        print(sw(j).toString + " <---> " + peek(c.io.oSum.bits).toString() + "   ")
+        if (sw(j) == peek((c.io.oSum.bits))) {
+          println("pass")
+        } else {
+          println("FAID")
+        }
 
-  // fourth let PE in getdata state
-  poke(c.io.stateSW, 1)
-  step(filter.length)
-
-  // finial let PE in cal state
-  poke(c.io.stateSW, 2)
-  step(1)
-  var j = 0
-  poke(c.io.oSum.ready, 1)
-  for (i <- Range(0, 20000)) {
-    //    println("oSum.bits: " + peek(c.io.oSum.valid).toString())
-    //    println("sw.cols: " + sw.cols.toString())
-    //    println("sw.rows: " + sw.rows.toString)
-    if (peek(c.io.oSum.valid) == 1) {
-      //      println(peek(c.io.oSum.bits).toString())
-      expect(c.io.oSum.bits, sw(j))
-      print(sw(j).toString + " <---> " + peek(c.io.oSum.bits).toString() + "   ")
-      if (sw(j) == peek((c.io.oSum.bits))) {
-        println("pass")
-      } else {
-        println("FAID")
+        //      println(s"${j} test pass")
+        j = j + 1
       }
-
-      //      println(s"${j} test pass")
-      j = j + 1
+      step(1)
     }
     step(1)
-  }
-  step(1)
-  assert(j == sw.length)
-  println("j: " + j.toString)
+    println("j: " + j.toString)
+    println("sw len: " + sw.length.toString)
+    assert(j == sw.length)
+    loop -= 1
+    reset(50)
+  }while(loop > 0)
 }
 
 class PETester extends ChiselFlatSpec {
@@ -317,7 +343,7 @@ class PETopModeTester extends ChiselFlatSpec {
         "--backend-name", "verilator"),
       () => new PETesterTop
     ) {
-      c => new testModen(c, filter, filterNum, img, imgNum, nchannel)
+      c => new testModen(c, 500)
     } should be(true)
     //    new File("test_run_dir/make_PETOPmode0_vcd/PETesterTop.vcd").exists should be(true)
 
