8cad0a50b4da8f5c854a27e438081ad3f04481c7
Qiwei Sun <1104439112@qq.com>
2019-09-07 22:46:58 +0800

    添加饱和计算模块

---
+++ src/main/scala/myutil/saturation.scala
@@ -0,0 +1,56 @@
+package myutil
+
+import chisel3._
+import chisel3.util._
+
+class Saturation(inw: Int = 16, outw: Int = 8, low: Int = 2) extends Module {
+  val io = IO(new Bundle {
+    val dataIn = Input(SInt(inw.W))
+    val dataOut = Output(SInt(outw.W))
+  })
+
+  val yuanma = Wire(UInt((inw - 1).W))
+  val sign = WireInit(io.dataIn(inw - 1))
+  val slice = Wire(UInt((inw - 1).W))
+  val carry = Wire(UInt(1.W))
+
+  when(sign === 1.U) {
+    // fu shu
+    yuanma := (~io.dataIn(inw - 1, 0)).asUInt() + 1.U
+  }.otherwise {
+    // zheng shu
+    yuanma := io.dataIn(inw - 1, 0)
+  }
+  slice := yuanma(inw - 1 - 1, low)
+
+  when(yuanma(low - 1) === 1.U) {
+    carry := 1.U
+  }.otherwise {
+    carry := 0.U
+  }
+  val out = Wire(new Bundle {
+    val sign = UInt(1.W)
+    val bits = UInt((outw - 1).W)
+  })
+  out.sign := 0.U
+  out.bits := 0.U
+  when(sign === 1.U) {
+    when((slice + carry).asUInt() >= 128.U) {
+      io.dataOut := (-128).asSInt(outw.W)
+    }.otherwise {
+      out.sign := sign
+      out.bits := (~(slice + carry)).asUInt() + 1.U
+      io.dataOut := out.asUInt().asSInt()
+    }
+  }.otherwise {
+    when((slice + carry).asUInt() >= 127.U) {
+      io.dataOut := 127.asSInt(outw.W)
+    }.otherwise {
+      out.sign := sign
+      out.bits := (slice + carry).asUInt()
+      io.dataOut := out.asUInt().asSInt()
+    }
+  }
+
+
+}
---
+++ src/test/scala/myutil/saturationTest.scala
@@ -0,0 +1,48 @@
+package myutil
+
+import chisel3._
+import chisel3.util._
+import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+
+class SaturationTest(c: Saturation) extends PeekPokeTester(c) {
+  def sw(x: Int): Int = {
+    val tmp = if (x >= 0) {
+      x / 4.0 + 0.5
+    } else {
+      x / 4.0 - 0.5
+    }
+    if (tmp >= 127) {
+      127
+    } else if (tmp <= -128) {
+      -128
+    } else {
+      tmp.toInt
+    }
+  }
+  var in = 11604
+  poke(c.io.dataIn, in)
+  expect(c.io.dataOut, sw(in))
+  println(in.toString + ":" + peek(c.io.dataOut).toString() + " <---> " + sw(in))
+  step(1)
+  val R = scala.util.Random
+  for(i <- Range(0, 100)){
+//    val in = R.nextInt(32767) - 16384
+    val in = R.nextInt(1024) - 512
+    poke(c.io.dataIn, in)
+    expect(c.io.dataOut, sw(in))
+    println(in.toString + ":" + peek(c.io.dataOut).toString() + " <---> " + sw(in))
+    step(1)
+  }
+}
+
+class SaturationTester extends ChiselFlatSpec {
+  "running with --generate-vcd-output on" should "create a vcd file from your test" in {
+    iotesters.Driver.execute(
+      Array("--generate-vcd-output", "on", "--target-dir", "test_run_dir/make_saturation_vcd",
+        "--top-name", "make_saturation__vcd", "--backend-name", "verilator"),
+      () => new Saturation()
+    ) {
+      c => new SaturationTest(c)
+    } should be(true)
+  }
+}
